63ac5d4923d7c5590ca94da9d09f4c7dd222de62
Barbara Morantes <barbara.morantes@thoughtworks.com>
2025-07-24 11:27:41 -0500

    Update jest.setup.js

--- 04-gif-expert-app/jest.setup.js
+++ 04-gif-expert-app/jest.setup.js
@@ -1,3 +1,3 @@
 import 'whatwg-fetch';
-import.meta.env.VITE_API_URL = 'https://api.giphy.com/v1';
-import.meta.env.VITE_GIPHY_API_KEY = 'a8Cf8nNcqrmfxNrihaLuE2dgaw1M6KuJ';
+import.meta.env.VITE_API_URL = 'your_api_url';
+import.meta.env.VITE_GIPHY_API_KEY = 'your_api_key';
